8bb9506d8e6f71a9e7da1e21173142aac0942a21
Maxime Kermarquer <maxime.kermarquer@icm-institute.org>
2018-03-07 14:07:15 +0100

    Update scheduling policy in job_submit.lua

--- job_submit.lua
+++ job_submit.lua
@@ -1,3 +1,162 @@
+ 
+-- 2018-03-07  
+-- ICM-Institute
+
+
+function classify_job (time, memory) 
+    -- QOS list
+    -- 1) qos1    --> 100%
+    -- 2) qos2    --> 80%
+    -- 3) qos3    --> 50%
+    -- 4) qos4    --> 30%
+    -- 5) qos5    --> 20%  
+   
+    -- Use case 
+    --  1) job.mem < 4G     job.time < 2h    --> qos1 
+    --  2) job.mem < 8G     job.time < 2h    --> qos1
+    --  3) job.mem < 16G    job.time < 2h    --> qos1 
+    --  4) job.mem < 32G    job.time < 2h    --> qos1
+    --  5) job.mem < 64G    job.time < 2h    --> qos1
+    --  6) job.mem < 122G   job.time < 2h    --> qos2
+    --
+    --  7) job.mem < 4G     job.time < 168h  --> qos2
+    --  8) job.mem < 8G     job.time < 168h  --> qos2
+    --  9) job.mem < 16G    job.time < 168h  --> qos2
+    -- 10) job.mem < 32G    job.time < 168h  --> qos2 
+    -- 11) job.mem < 64G    job.time < 168h  --> qos3
+    -- 12) job.mem < 122G   job.time < 168h  --> qos3
+    --
+    --  7) job.mem < 4G     job.time < 336h  --> qos2
+    --  8) job.mem < 8G     job.time < 336h  --> qos2
+    --  9) job.mem < 16G    job.time < 336h  --> qos2
+    -- 10) job.mem < 32G    job.time < 336h  --> qos2 
+    -- 11) job.mem < 64G    job.time < 336h  --> qos3
+    -- 12) job.mem < 122G   job.time < 336h  --> qos3
+    --
+    -- 13) job.mem < 4G     job.time < 2232h --> qos4 
+    -- 14) job.mem < 8G     job.time < 2232h --> qos4
+    -- 15) job.mem < 16G    job.time < 2232h --> qos4
+    -- 16) job.mem < 32G    job.time < 2232h --> qos4 
+    -- 17) job.mem < 64G    job.time < 2232h --> qos5 
+    -- 18) job.mem < 122G   job.time < 2232h --> qos5
+
+    -- Job shorter than 2 hours
+    if (time <= 120) then
+    	if (memory <= 4096) then 
+    		return "qos1"
+    	end
+
+    	if (memory <= 8192) then 
+    		return "qos1"
+    	end
+
+    	if (memory <= 16384) then 
+    		return "qos1"
+    	end
+
+    	if (memory <= 32768) then 
+    		return "qos1"
+    	end
+
+    	if (memory <= 65536) then 
+    		return "qos1"
+    	end
+
+    	if (memory <= 124928) then 
+    		return "qos2"
+    	end
+    end
+    
+    -- Job shorter than one week
+    if (time <= 10080) then
+    	if (memory <= 4096) then 
+    		return "qos2"
+    	end
+
+    	if (memory <= 8192) then 
+    		return "qos2"
+    	end
+
+    	if (memory <= 16384) then 
+    		return "qos2"
+    	end
+
+    	if (memory <= 32768) then 
+    		return "qos2"
+    	end
+
+    	if (memory <= 65536) then 
+    		return "qos3"
+    	end
+
+    	if (memory <= 124928) then 
+    		return "qos3"
+    	end
+    end
+
+
+    -- Job shorter than two week
+    if (time <= 20160) then
+    	if (memory <= 4096) then 
+    		return "qos3"
+    	end
+
+    	if (memory <= 8192) then 
+    		return "qos3"
+    	end
+
+    	if (memory <= 16384) then 
+    		return "qos3"
+    	end
+
+    	if (memory <= 32768) then 
+    		return "qos4"
+    	end
+
+    	if (memory <= 65536) then 
+    		return "qos5"
+    	end
+
+    	if (memory <= 124928) then 
+    		return "qos5"
+    	end
+    end
+
+    -- Job shorter than three months (84 days)
+    if (time <= 120960) then
+    	if (memory <= 4096) then 
+    		return "qos4"
+    	end
+
+    	if (memory <= 8192) then 
+    		return "qos4"
+    	end
+
+    	if (memory <= 16384) then 
+    		return "qos4"
+    	end
+
+    	if (memory <= 32768) then 
+    		return "qos4"
+    	end
+
+    	if (memory <= 65536) then 
+    		return "qos5"
+    	end
+
+    	if (memory <= 124928) then 
+    		return "qos5"
+    	end
+    end
+
+
+    return "qos5"
+end
+
+
+function slurm_job_modify(job_desc, job_rec, part_list, modify_uid) 
+        return slurm.SUCCESS 
+end 
 
 
 function slurm_job_submit(job_desc, part_list, submit_uid)
@@ -31,12 +190,12 @@ function slurm_job_submit(job_desc, part_list, submit_uid)
         end
     end
       
-    
-
     slurm.log_info("time_limit %d", job_desc.time_limit)
     slurm.log_info("pn_min_memory %s", job_desc.pn_min_memory)
+
     -- Classify the job        
     job_desc.qos = classify_job (job_desc.time_limit, job_desc.pn_min_memory)
+
     slurm.log_info ("Job classify in qos %s", job_desc.qos)
 
       
@@ -48,121 +207,10 @@ function slurm_job_submit(job_desc, part_list, submit_uid)
     return slurm.SUCCESS 
 
 end 
- 
-
-function classify_job (time, memory) 
-    -- QOS list
-    -- 1) qos1    --> 100%
-    -- 2) qos2    --> 80%
-    -- 3) qos3    --> 70%
-    -- 4) qos4    --> 50%
-    -- 5) qos5    --> 40% 
-    -- 6) special --> 100% 
-   
-    -- Use case 
-    --  1) job.mem < 4G     job.time < 2h    --> qos1 
-    --  2) job.mem < 8G     job.time < 2h    --> qos1
-    --  3) job.mem < 16G    job.time < 2h    --> qos1 
-    --  4) job.mem < 32G    job.time < 2h    --> qos1
-    --  5) job.mem < 64G    job.time < 2h    --> qos1
-    --  6) job.mem < 122G   job.time < 2h    --> qos3
-    --
-    --  7) job.mem < 4G     job.time < 168h  --> qos2
-    --  8) job.mem < 8G     job.time < 168h  --> qos2
-    --  9) job.mem < 16G    job.time < 168h  --> qos2
-    -- 10) job.mem < 32G    job.time < 168h  --> qos2 
-    -- 11) job.mem < 64G    job.time < 168h  --> qos3
-    -- 12) job.mem < 122G   job.time < 168h  --> qos4
-    --
-    -- 13) job.mem < 4G     job.time < 2232h --> qos4 
-    -- 14) job.mem < 8G     job.time < 2232h --> qos4
-    -- 15) job.mem < 16G    job.time < 2232h --> qos4
-    -- 16) job.mem < 32G    job.time < 2232h --> qos4 
-    -- 17) job.mem < 64G    job.time < 2232h --> qos4 
-    -- 18) job.mem < 122G   job.time < 2232h --> qos5
-
-    -- Job shorter than 2 hours
-    if (memory <= 4096) and (time <= 120) then
-        return "qos1"
-    end
-    
-    if (memory <= 8192) and (time <= 120) then
-        return "qos1"
-    end
-    
-    if (memory <= 16384) and (time <= 120) then
-        return "qos1"
-    end
-
-    if (memory <= 32768) and (time <= 120) then
-        return "qos1"
-    end
-
-    if (memory <= 65536) and (time <= 120) then
-        return "qos1"
-    end
-    
-    if (memory <= 124928) and (time <= 120) then
-        return "qos3"
-    end
-
-    -- Job shorter than one week
-    if (memory <= 4096) and (time <= 10080) then
-        return "qos2"
-    end
-    
-    if (memory <= 8192) and (time <= 10080) then
-        return "qos2"
-    end
-    
-    if (memory <= 16384) and (time <= 10080) then
-        return "qos2"
-    end
-
-    if (memory <= 32768) and (time <= 10080) then
-        return "qos2"
-    end
-
-    if (memory <= 65536) and (time <= 10080) then
-        return "qos3"
-    end
-    
-    if (memory <= 124928) and (time <= 10080) then
-        return "qos4"
-    end
-
-    -- Job shorter than 84 days
-    if (memory <= 4096) and (time <= 120960) then
-        return "qos4"
-    end
-    
-    if (memory <= 8192) and (time <= 120960) then
-        return "qos4"
-    end
-    
-    if (memory <= 16384) and (time <= 120960) then
-        return "qos4"
-    end
-
-    if (memory <= 32768) and (time <= 120960) then
-        return "qos4"
-    end
-
-    if (memory <= 65536) and (time <= 120960) then
-        return "qos4"
-    end
-    
-    if (memory <= 124928) and (time <= 120960) then
-        return "qos5"
-    end
-
-    return "none"
-end
 
 
-function slurm_job_modify(job_desc, job_rec, part_list, modify_uid) 
-        return slurm.SUCCESS 
-end 
+
+
 
 slurm.log_info("LUA/JobSubmit :: initialized") 
 return slurm.SUCCESS 
